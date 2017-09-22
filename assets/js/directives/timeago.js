5bcd13f910f2314b3b6e68b567d27974ae50b3c2
STAR-COMMAND\Owner <fassetar@gmail.com>
2017-09-21 23:26:02 -0400

    missed files

--- assets/js/directives/timeago.js
+++ assets/js/directives/timeago.js
@@ -1,18 +1,24 @@
-#= require jquery-timeago/jquery.timeago
+var module;
 
+module = angular.module("plunker.timeago", []);
 
-module = angular.module "plunker.timeago", []
-
-module.directive "timeago", [ "$timeout", "$filter", ($timeout, $filter) ->
-  restrict: "A"
-  replace: true
-  link: ($scope, $el, $attrs) ->
-    $attrs.$observe "timeago", (date) ->
-      if date
-        date = if angular.isDate(date) then date else new Date(date)
-        
-        $el.text $filter("date")(date, "medium")
-        $attrs.$set "title", date.toISOString()
-        
-        $timeout -> $el.timeago()
-]
+module.directive("timeago", [
+  "$timeout", "$filter", function($timeout, $filter) {
+    return {
+      restrict: "A",
+      replace: true,
+      link: function($scope, $el, $attrs) {
+        return $attrs.$observe("timeago", function(date) {
+          if (date) {
+            date = angular.isDate(date) ? date : new Date(date);
+            $el.text($filter("date")(date, "medium"));
+            $attrs.$set("title", date.toISOString());
+            return $timeout(function() {
+              return $el.timeago();
+            });
+          }
+        });
+      }
+    };
+  }
+]);
--- assets/js/services/stream.js
+++ assets/js/services/stream.js
@@ -1,10 +1,17 @@
-module = angular.module "plunker.stream", [
-  "firebase"
-]
+var module;
 
-module.service "stream", [ () ->
-  new class Stream
-  
-    connect: (streamId) ->
-      
-]
+module = angular.module("plunker.stream", ["firebase"]);
+
+module.service("stream", [
+  function() {
+    var Stream;
+    return new (Stream = (function() {
+      function Stream() {}
+
+      Stream.prototype.connect = function(streamId) {};
+
+      return Stream;
+
+    })());
+  }
+]);
