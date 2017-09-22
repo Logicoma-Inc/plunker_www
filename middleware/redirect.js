6af1ec3af31dac06fdbc4fa9b8bb171e3bdedab5
Åℵ┬ℏÔñÝ ℉⅍§ë‡ <fassetar@users.noreply.github.com>
2017-09-21 20:11:20 -0400

    Update and rename redirect.coffee to redirect.js

---
+++ middleware/redirect.js
@@ -0,0 +1,15 @@
+module.exports.middleware = function(options) {
+  if (options == null) {
+    options = {};
+  }
+  return function(req, res, next) {
+    if (!(options.from && options.to)) {
+      return next();
+    }
+    if (req.get('host') === options.from) {
+      return res.redirect("http://" + options.to + req.url);
+    } else {
+      return next();
+    }
+  };
+};
